426708366912273297fc9fbbd752fe5efc6caa6a
evgenykochkov <crabbs100@gmail.com>
2022-09-19 21:58:55 +0300

    fixed errors

--- app/src/main/java/com/ekochkov/santehnikatestapp/view/fragment/MapFragment.kt
+++ app/src/main/java/com/ekochkov/santehnikatestapp/view/fragment/MapFragment.kt
@@ -11,6 +11,7 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.ekochkov.santehnikatestapp.R
 import com.ekochkov.santehnikatestapp.databinding.FragmentMapBinding
 import com.ekochkov.santehnikatestapp.utils.Constants
 import com.google.android.gms.location.FusedLocationProviderClient
